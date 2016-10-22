57b3d42a3f3781960c5eb65016b12fe2eb5755c9
rachely3n <ry88@cornell.edu>
2016-10-22 15:49:45 -0700

    Multiplication is working, i jsut need to optimize the previous non-backward methods using indices i + j

---
+++ arrays/multiplication_6_3.py
@@ -0,0 +1,75 @@
+def isneg(n):
+  return n < 0
+
+def multiply(a, b):
+  n_a = len(a)
+  n_b = len(b)
+  n_res = n_a + n_b
+  res = [0] * (n_res)
+  # too many variables? probably 
+  res_idx = 0
+  temp = 0
+  carry = 0 
+  neg = False 
+  
+  #take care of negation
+  #when you wish you had ternary operators :(
+  if isneg(a[0]) or isneg(b[0]):
+    if not (isneg(a[0]) and isneg(b[0])):
+      neg = True 
+    if isneg(a[0]):
+      a[0] *= -1
+    if isneg(b[0]):
+      b[0] *= -1
+
+
+  # actual mutiplying
+  for i in range(n_a):
+    # fill the result at the right place.
+    # result should start wherever a starts
+    res_idx = n_res - i - 1
+    for j in range(n_b):
+      temp = a[n_a - 1 - i] * b[n_b - 1 - j]
+      res[res_idx] += temp 
+      if res[res_idx] >= 10: 
+        temp = res[res_idx]
+        carry = temp / 10
+        res[res_idx] %= 10 
+        res[res_idx - 1] += carry
+      res_idx -= 1
+
+  # get rid of leading 0's 
+  write_idx = 0
+  last_zero = True
+  for i in range(n_res):
+    if res[i] == 0 and last_zero:
+      write_idx += 1
+    else:
+      break 
+
+  if neg:
+    res[write_idx] *= -1
+  return res[write_idx:]
+
+print multiply([1],[2])
+print multiply([-9,9], [-9,9])
+print multiply([1,6], [1,6])
+
+def epi_multiply(a,b):
+  a = a[::-1]
+  b = b[::-1]
+  n_a = len(a)
+  n_b = len(b)
+  n_res = n_a + n_b
+  res = [0 for i in xrange(n_res)]
+
+  for i in xrange(n_a):
+    for j in xrange(n_b):
+      res[i + j] += a[i] * b[j]
+      res[i + j + 1] += res[i + j] / 10 #never out of range because of 0-indexing 
+      res[i + j] %= 10 
+
+  return res[::-1]
+
+print epi_multiply([1],[2])
+print epi_multiply([9,9], [9,9])
